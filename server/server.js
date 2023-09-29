2c651018269935cf2a4ad604df91a79d4a2817e0
Christopher Holt <holt.christopher1@gmail.com>
2023-09-29 14:23:57 +1000

    adding render website to the port in .env

--- server/server.js
+++ server/server.js
@@ -28,5 +28,5 @@ app.get('/*', function(req, res) {
 });
 
 app.listen(port, function() {
-console.log(`Express app running on port ${port}`)
+  console.log(`Express app running on port ${port}`)
 });
